68d0232c9544e25c40da003ea504ac8caa43e356
Cody Cutler <ccutler@csail.mit.edu>
2014-11-03 23:48:51 -0500

    checkpoint
    functional

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+import hashlib
+import sys
+
 def lump(pre, post):
     return [pre + i for i in post]
 
@@ -47,6 +50,30 @@ class Tran:
         self.effs.append(l)
     def addout(self, l):
         self.out.append(l)
+
+    def prpre(self, idts):
+        if not self.pres:
+            print '\t'*idts + 'return False'
+
+        for i in self.pres:
+            print '\t'*idts + i
+
+    def preff(self, idts):
+        if not self.effs:
+            print '\t'*idts + 'pass'
+
+        for i in self.effs:
+            print '\t'*idts + i
+
+    def prout(self, idts):
+        # outputs can be omitted
+        if not self.out:
+            #print '\t'*idts + 'raise Exception("no imp")'
+            print '\t'*idts + 'return None'
+
+        for i in self.out:
+            print '\t'*idts + i
+
     def setname(self, l):
         self.name = l
 
@@ -56,13 +83,45 @@ class Parser:
         self.state = states.START
         self.auto = newauto()
 
-    def dump(self):
-        print 'this will print an automaton soon'
+    def addtoret(self, ntd, idts):
+        print '\t'*idts + 'd = {',
+        for k in ntd:
+            print '"' + k + '": %s,' % (ntd[k]),
 
-        print self.auto
-        print 'trans'
-        for i in self.auto['trans']:
-            print i
+        print '}'
+        print '\t'*idts + 'ret.append(d)'
+
+    def dump(self):
+        get = lambda x: self.auto[x]
+
+        print 'class %s:' % (get('name'))
+        print
+        print '\tdef __init__(self):'
+        print '\t\tpass'
+        print
+        print '\tdef init(self):'
+        print ''.join(['\t\t' + i + '\n' for i in get('state')])
+        print
+        print '\tdef actions(self):'
+        print '\t\tret = []'
+        for t in get('trans'):
+            prec, arity = self.protoprint(t, 'prec', 2)
+            ntd = {'name': '"' + t.name + '"', 'prec': prec, 'arityprec': arity}
+            t.prpre(3)
+
+            eff, arity = self.protoprint(t, 'eff', 2)
+            ntd['eff'] = eff
+            ntd['arityeff'] = arity
+            t.preff(3)
+
+            op, arity = self.protoprint(t, 'output', 2)
+            ntd['output'] = op
+            ntd['arityoutput'] = arity
+            t.prout(3)
+
+            self.addtoret(ntd, 2)
+
+        print '\t\treturn ret'
 
     def enterstate(self, s):
         if s == states.SIGINP:
@@ -106,6 +165,10 @@ class Parser:
                      states.TASKS   : self.pptasks,
                      states.TRANNAM : self.pptrannam,
                      }
+
+        if l and l[0] == '#':
+            return None
+
         s = self.nextstate(l)
 
         if s:
@@ -124,13 +187,14 @@ class Parser:
         pass
 
     def ppsigin(self, l):
-        self.auto['sigin'].append(l)
+        # strip parens and args from sigs
+        self.auto['sigin'].append(l.split('(')[0])
 
     def ppsigint(self, l):
-        self.auto['sigint'].append(l)
+        self.auto['sigint'].append(l.split('(')[0])
 
     def ppsigout(self, l):
-        self.auto['sigout'].append(l)
+        self.auto['sigout'].append(l.split('(')[0])
 
     def pptranpre(self, l):
         self.auto['trans'][-1].addpre(l)
@@ -150,6 +214,43 @@ class Parser:
     def pptrannam(self, l):
         self.auto['trans'].append(Tran())
         self.auto['trans'][-1].setname(l)
+
+    def protoprint(self, t, ty, idts):
+        '''
+        prints the prototype with the correct argument (as needed by the
+        simulator) and returns the name of the method for inclusion in the list
+        of actions.
+
+        ty is the type: it is one of prec, eff, output (precondition, effect,
+        and output respectively)
+        '''
+
+        if ty not in ['prec', 'eff', 'output']:
+            raise ValueError('unknown transition type %s' % (ty))
+
+        get = lambda x: self.auto[x]
+        n =  t.name
+        lit =  n.split('(')[0]
+        args = ''.join(''.join(n.split('(')[1:]).split(')')).split(',')
+        sigins = get('sigin')
+        sigints = get('sigint')
+        sigouts = get('sigout')
+
+        arity = len(args)
+
+        if lit in sigins:
+            args.insert(0, 'fr')
+            arity += 1
+        elif lit in sigouts:
+            args.insert(0, 'to')
+            arity += 1
+
+        #hs = hashlib.md5(lit).hexdigest()
+        #print '\t'*idts + 'def %s_%s%s:' % (lit, hs, ''.join(args))
+        lit = '%s_%s' % (lit, ty)
+        print '\t'*idts + 'def %s(%s):' % (lit, ','.join(args))
+
+        return lit, arity
 
     def nextstate(self, l):
 
@@ -180,4 +281,4 @@ if __name__ == '__main__':
     with open('test.txt', 'r') as f:
         p.go(f)
 
-    print 'done'
+    print >> sys.stderr, 'done'
